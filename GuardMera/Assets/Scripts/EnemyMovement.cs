b0ecc54bd5b0cde98b1c72b4833e121fcc0dc3df
Jexatious <jadikah10@gmail.com>
2026-04-06 17:10:20 -0400

    Restoring Project Files to EnemyMovment and Waypoint Incorporation

--- GuardMera/Assets/Scripts/EnemyMovement.cs
+++ GuardMera/Assets/Scripts/EnemyMovement.cs
@@ -1,16 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class EnemyMovement : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private Transform target;
+    private int waypointIndex = 0;
+    //private Enemy enemy;
+
+    [SerializeField]
+    private float changeDirThreshold = 0.4f;
+
+    [Header("Test Values")]
+    public float speed = 10f;
+
     void Start()
     {
-        
+        target = Waypoints.points[0];
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Vector2 dir = target.position - transform.position;
+        transform.Translate(dir.normalized * speed * Time.deltaTime, Space.World);
+        if(Vector2.Distance(transform.position, target.position) <= changeDirThreshold)
+        {
+            GetNextWayPoint();
+        }
+    }
+
+    void GetNextWayPoint()
+    {
+        if (waypointIndex >= Waypoints.points.Length - 1)
+        {
+            PathEnd();
+            return;
+        }
+
+        waypointIndex++;
+        target = Waypoints.points[waypointIndex];
+    }
+
+    void PathEnd()
+    {
+        Destroy(gameObject);
     }
 }
